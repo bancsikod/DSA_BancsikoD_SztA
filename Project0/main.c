fe5c4dea1ba4a41171174edc38de56ca7fbb7530
bancsikod <bancsikod@yahoo.com>
2021-02-17 16:15:24 +0200

    Ket szam osszege.

--- Project0/main.c
+++ Project0/main.c
@@ -4,19 +4,26 @@
 //megadott két egész szám összegét.
 #include <stdio.h>
 
-int sum(int , int );
+//int sum(int , int );
+void sum(int , int , int *);
 
 int main() {
-    int a,b,aplusb;
+    int a, b, aplusb;
     printf("add meg a szamokat:");
     scanf("%i%i", &a, &b);
-    aplusb = sum(a,b);
+    //aplusb = sum(a,b);
+    sum(a, b, &aplusb);
     printf("a ket szam osszege = %i", aplusb);
     return 0;
 }
 
-int sum(int a, int b) {
+void sum(int a, int b, int *aplusb) {
+    int s;
+    *aplusb = a + b;
+}
+
+/*int sum(int a, int b) {
     int s;
     s = a + b;
     return s;
-}
+}*/
