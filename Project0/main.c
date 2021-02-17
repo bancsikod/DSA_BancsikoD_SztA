4557ca8712db12ef662025343e91b4f32363ee54
bancsikod <bancsikod@yahoo.com>
2021-02-17 15:58:44 +0200

    Ket szam osszege.

--- Project0/main.c
+++ Project0/main.c
@@ -1,6 +1,22 @@
+// Írj egy egyszerű programot (csak a main.c fájlt használd), amely kiszámolja két szám
+//összegét. Két szám összegének meghatározásához használj függvényt (írd meg a
+//függvény deklarációját és definícióját is). A függvény térítse vissza a paraméterként
+//megadott két egész szám összegét.
 #include <stdio.h>
 
+int sum(int , int );
+
 int main() {
-    printf("elso push to git\n");
+    int a,b,aplusb;
+    printf("add meg a szamokat:");
+    scanf("%i%i", &a, &b);
+    aplusb = sum(a,b);
+    printf("a ket szam osszege = %i", aplusb);
     return 0;
+}
+
+int sum(int a, int b) {
+    int s;
+    s = a + b;
+    return s;
 }
