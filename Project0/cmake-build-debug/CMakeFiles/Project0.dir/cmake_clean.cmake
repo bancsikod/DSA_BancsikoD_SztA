0236d390841e510347f6434b6466cd6c3a92cef2
bancsikod <bancsikod@yahoo.com>
2021-02-17 15:47:57 +0200

    Elso commit & push.

---
+++ Project0/cmake-build-debug/CMakeFiles/Project0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project0.dir/main.c.obj"
+  "Project0.exe"
+  "Project0.exe.manifest"
+  "Project0.pdb"
+  "libProject0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Project0/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("elso push to git\n");
+    return 0;
+}
