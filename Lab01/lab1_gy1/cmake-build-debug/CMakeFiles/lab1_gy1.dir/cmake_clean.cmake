f024aa8aa2d526901e229881a975aea0b6aec64b
bancsikod <bancsikod@yahoo.com>
2021-02-24 15:47:16 +0200

    feladat1

---
+++ Lab01/lab1_gy1/cmake-build-debug/CMakeFiles/lab1_gy1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_gy1.dir/function.c.obj"
+  "CMakeFiles/lab1_gy1.dir/main.c.obj"
+  "lab1_gy1.exe"
+  "lab1_gy1.exe.manifest"
+  "lab1_gy1.pdb"
+  "liblab1_gy1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1_gy1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab01/lab1_gy1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "cmake-build-debug/function.h"
+
+int main() {
+    printf("min = %i", min(63,21,75));
+    printf("\nmax = %i", max(63,21,75));
+    return 0;
+}
